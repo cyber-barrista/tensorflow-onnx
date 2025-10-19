ae24c539be5b65b58ce643aae55b1c0812282c26
Daniil Chalov <dchalov@evolution.com>
2025-10-19 22:22:04 +0200

    Update protobuf to 6.33

--- tests/utils/setup_test_env.sh
+++ tests/utils/setup_test_env.sh
@@ -26,7 +26,7 @@ pip install "tensorflow-text<=$TF_VERSION"
 pip uninstall -y tensorflow
 pip install tensorflow==$TF_VERSION
 pip uninstall -y protobuf
-pip install "protobuf~=4.25"
+pip install "protobuf~=6.33"
 
 python setup.py install
 
